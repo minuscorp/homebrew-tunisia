55e3ed56ae47c41d0369f093b6b4a2abec0926d2
Jorge Revuelta <minuscorp@gmail.com>
2020-07-29 19:58:31 +0200

    Bump version

--- tunisia.rb
+++ tunisia.rb
@@ -1,8 +1,8 @@
 class Tunisia < Formula
   desc "A Carthage cache dependant on Swift, Xcode and library version."
   homepage "https://github.com/minuscorp/Tunisia"
-  url "https://github.com/minuscorp/Tunisia/archive/v0.0.1.tar.gz"
-  sha256 "af405f183981a3ab87e30553855c92641a11c26f332234919cab7e613f1c4208"
+  url "https://github.com/minuscorp/Tunisia/archive/v0.0.2.tar.gz"
+  sha256 "d3382deefa6a282b1218cbbf56581fc48642c388f6bb3154ae5bdca63e4f1340"
 
   depends_on xcode: ["11.2", :build, :test]
 
